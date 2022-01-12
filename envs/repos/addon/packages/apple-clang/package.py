606d9389696ee444d243d757e19e04a5745a79f1
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-01-12 16:40:53 -0700

    Clean up apple-clang package in addon repo

--- envs/repos/addon/packages/apple-clang/package.py
+++ envs/repos/addon/packages/apple-clang/package.py
@@ -7,21 +7,13 @@ from spack import *
 
 
 class AppleClang(Package):
-    """FIXME: Put a proper description of your package here."""
+    """Meta package to 'install' the native Apple clang compiler in spack."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://opensource.apple.com/projects/llvm-clang"
-    url      = "https://www.notavalidurl.com/notavalidfile.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
     maintainers = ['climbfuji']
 
-    # FIXME: Add proper versions here.
     version('13.0.0')
-
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
 
     def install(self, spec, prefix):
         raise InstallError("apple-clang is a meta package that cannot be installed")
