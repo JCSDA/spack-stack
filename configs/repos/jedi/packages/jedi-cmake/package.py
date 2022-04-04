e9a7b26335ee550a94fb68118171a2e823f2449c
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-04-04 08:10:48 -0600

    Switch to jedi-cmake 1.3.0 in JCSDA public repository

--- configs/repos/jedi/packages/jedi-cmake/package.py
+++ configs/repos/jedi/packages/jedi-cmake/package.py
@@ -11,15 +11,16 @@ class JediCmake(CMakePackage):
 
     homepage = "https://github.com/JCSDA-internal/jedi-cmake"
     git = "https://github.com/JCSDA-internal/jedi-cmake.git"
-    url = "https://github.com/JCSDA-internal/jedi-cmake/archive/1.0.0.zip"
+    url = "https://github.com/JCSDA/jedi-cmake/archive/refs/tags/1.3.0.tar.gz"
 
-    maintainers = ['rhoneyager']
+    maintainers = ['climbfuji', 'rhoneyager']
 
     version('master', branch='master', no_cache=True)
     version('develop', branch='develop', no_cache=True)
-    version('1.2.0', url="https://github.com/JCSDA/jedi-cmake/archive/refs/tags/1.2.0.tar.gz", sha256='eb9f1c403d1b43a90a5e774097382b183d56d5b40a1204b51af2da8db1559b21', preferred=True)
-    version('1.1.0', url="https://github.com/JCSDA/jedi-cmake/archive/refs/tags/1.1.0.tar.gz", sha256='f1fe41eb5edd343bdf57eb76bea6d1b9f015878f0a9d0eb1e9dba18b903d3b35')
-    version('1.0.0', url="https://github.com/JCSDA/jedi-cmake/archive/refs/tags/1.0.0.tar.gz", sha256='d773a800350e69372355b45e89160b593818cd438a86925b8a689c47996a0b9a')
+    version('1.3.0', sha256='b217e2250398f6c34f0da0a50a8efe500684af4d484adebaa87a9de630eee1b7', preferred=True)
+    version('1.2.0', sha256='eb9f1c403d1b43a90a5e774097382b183d56d5b40a1204b51af2da8db1559b21')
+    version('1.1.0', sha256='f1fe41eb5edd343bdf57eb76bea6d1b9f015878f0a9d0eb1e9dba18b903d3b35')
+    version('1.0.0', sha256='d773a800350e69372355b45e89160b593818cd438a86925b8a689c47996a0b9a')
 
     depends_on('cmake @3.10:', type=('build'))
 
