5dc49f457e4c24ebdeba62c1f5afb9f738e3d459
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-04-18 09:56:27 -0600

    Update configs/repos/jedi/packages/jedi-ufs-bundle-env/package.py

--- configs/repos/jedi/packages/jedi-ufs-bundle-env/package.py
+++ configs/repos/jedi/packages/jedi-ufs-bundle-env/package.py
@@ -11,9 +11,9 @@ from spack import *
 class JediUfsBundleEnv(BundlePackage):
     """Development environment for fv3-bundle"""
 
-    # DH* TODO UPDATE
-    homepage = "https://github.com/JCSDA-internal/ufs-bundle"
-    git      = "https://github.com/JCSDA-internal/ufs-bundle.git"
+    # DH* TODO - we should rename this to just ufs-bundle to match the other bundles
+    homepage = "https://github.com/JCSDA/ufs-jedi-bundle"
+    git      = "https://github.com/JCSDA/ufs-jedi-bundle.git"
 
     maintainers = ['climbfuji', 'mark-a-potts']
 
@@ -23,20 +23,3 @@ class JediUfsBundleEnv(BundlePackage):
     depends_on('jedi-base-env', type='run')
 
     depends_on('ufs-weather-model-env', type='run')
-    #depends_on('jasper', type='run')
-    #depends_on('libpng', type='run')
-    #
-    #depends_on('fms', type='run')
-    #depends_on('esmf', type='run')
-    #
-    #depends_on('bacio',  type='run')
-    #depends_on('crtm',   type='run')
-    #depends_on('g2',     type='run')
-    #depends_on('g2tmpl', type='run')
-    #depends_on('ip',     type='run')
-    #depends_on('sp',     type='run')
-    #depends_on('w3nco',  type='run')
-    #
-    #depends_on('gftl-shared', type='run')
-    #depends_on('yafyaml',     type='run')
-    #depends_on('mapl',        type='run')
