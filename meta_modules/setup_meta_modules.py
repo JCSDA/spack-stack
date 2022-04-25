9000cf935a89bc6b7002e3c1f7775410156c5a58
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-04-25 13:03:03 -0600

    Finalize jedi-ewok on discover

--- meta_modules/setup_meta_modules.py
+++ meta_modules/setup_meta_modules.py
@@ -87,10 +87,6 @@ def get_matched_dict(root_dir, candidate_list, sub_candidate_list = None):
         matched_name = None
         matched_version = None
         for xdir in dirs:
-            logging.warning("DH DEBUG: candidate_list = '{}'".format(candidate_list))
-            logging.warning("DH DEBUG: candidate      = '{}'".format(candidate))
-            logging.warning("DH DEBUG: dirs           = '{}'".format(dirs))
-            logging.warning("DH DEBUG: xdir           = '{}'".format(xdir))
             # Partial matches
             if ('@' in candidate and xdir==candidate.split('@')[0]) or (xdir==candidate):
                 candidate_dir = os.path.join(root_dir, xdir)
