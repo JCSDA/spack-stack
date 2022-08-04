ab1d1ed93979cb05fbe3020dda7bbd9bc34c933c
Mark Potts <Mark.Potts@noaa.gov>
2022-08-04 16:09:26 -0400

    reverted changes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-# https://stackoverflow.com/questions/59895/how-can-i-get-the-.-directory-of-a-bash-script-from-within-the-script-itsel
+# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
 # Portable way to get current directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export SPACK_STACK_DIR=${SCRIPT_DIR}
 echo "Setting environment variable SPACK_STACK_DIR to ${SPACK_STACK_DIR}"
 
-. ${SCRIPT_DIR}/spack/share/spack/setup-env.sh
+source ${SCRIPT_DIR}/spack/share/spack/setup-env.sh
 echo "Sourcing spack environment ${SCRIPT_DIR}/spack/share/spack/setup-env.sh"
