bbacadccefdd72167785c6e5706d96f3f1fc644c
Alex Richert <alexander.richert@noaa.gov>
2023-06-27 01:26:54 -0700

    Allow show_duplicate_packages.py to ignore user-specified package names

--- util/show_duplicate_packages.py
+++ util/show_duplicate_packages.py
@@ -20,12 +20,13 @@ def colorize_spec(line, package_name, colorize=False):
     c1 = r'\033[93m' ; c2 = r'\033[0m'
     return re.sub("(\w{7}\s+)(%s)@"%package_name, f"\\1{c1}\\2{c2}@", line)
 
-def show_duplicate_packages(txt_to_check, only_show_dups=False):
+def show_duplicate_packages(txt_to_check, ignore_list=[], only_show_dups=False):
     dd = defaultdict(set)
     for line in txt_to_check.split("\n"):
         line = line.replace("^", "")
         package_name = re.findall("\s\w{7}\s+(\^?[^\s@]+)@", line)
         if not package_name: continue
+        if package_name[0] in ignore_list: continue
         line = "  ".join(line.split()[1:])
         dd[package_name[0]].add(line)
     duplicates_found = False
@@ -45,12 +46,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check output of `spack concretize` for duplicate packages")
     parser.add_argument("filename", nargs="?")
     parser.add_argument("-d", action="store_true")
+    parser.add_argument("-i", nargs="*", default=[])
     args = parser.parse_args()
     if args.filename:
         with open(args.filename, "r") as f:
             txt_to_check = f.read()
     else:
         txt_to_check = sys.stdin.read()
-    ret = show_duplicate_packages(txt_to_check, only_show_dups=args.d)
+    ret = show_duplicate_packages(txt_to_check, only_show_dups=args.d, ignore_list=args.i)
     sys.exit(ret)
 
