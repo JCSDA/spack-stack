4f5724c3c1fcaba7cc571abda79d64b10b6e4f8c
Alex Richert <alexander.richert@noaa.gov>
2023-07-03 16:07:40 -0700

    fix util CI for MacOS

--- util/util_tests.sh
+++ util/util_tests.sh
@@ -7,34 +7,40 @@ function run_and_check(){
   label=$2
   shift 2
   echo "Running '$*' in $PWD"
-  eval "$*" &> /dev/null
+  eval "$*" &> /tmp/output.$$
   if [ $? -ne $expected ]; then
-    echo "Test $label failed!"
+    echo "Test $label failed! Output:"
+    cat /tmp/output.$$
+    rm /tmp/output.$$
+    echo
     fail=1
   fi
 }
 
+echo "umask:" $(umask)
+chmod o+rX $HOME
 mkdir -p ${SPACK_STACK_DIR}/util/checks
 cd ${SPACK_STACK_DIR}/util/checks
 
 # Check check_permissions.sh
 mkdir -p perm_check1/perm_check2/perm_check3
 cd perm_check1/perm_check2
-chmod o+rX ../../perm_check1 -R
+chmod 777 ../../perm_check1
+chmod 777 .
+chmod 777 ./perm_check3
 run_and_check 0 "check_permissions A" ${SPACK_STACK_DIR}/util/check_permissions.sh
-chmod o-r ../../perm_check1
+chmod 776 ../../perm_check1
 run_and_check 1 "check_permissions B" ${SPACK_STACK_DIR}/util/check_permissions.sh
-chmod o-X ../../perm_check1
+chmod 773 ../../perm_check1
 run_and_check 1 "check_permissions C" ${SPACK_STACK_DIR}/util/check_permissions.sh
-chmod o+r ../../perm_check1
+chmod 770 ../../perm_check1
 run_and_check 1 "check_permissions D" ${SPACK_STACK_DIR}/util/check_permissions.sh
-chmod o+rX ../../perm_check1
-chmod o-rX perm_check3
+chmod 777 ../../perm_check1
+chmod 776 perm_check3
 run_and_check 1 "check_permissions E" ${SPACK_STACK_DIR}/util/check_permissions.sh
-chmod o+r perm_check3
+chmod 773 perm_check3
 run_and_check 1 "check_permissions F" ${SPACK_STACK_DIR}/util/check_permissions.sh
-chmod o-r perm_check3
-chmod o+X perm_check3
+chmod 770 perm_check3
 run_and_check 1 "check_permissions G" ${SPACK_STACK_DIR}/util/check_permissions.sh
 
 # Check show_duplicate_packages.py
