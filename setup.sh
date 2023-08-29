55e68343c6612f5f4bbe783f1684bab188caacd1
Alex Richert <alexander.richert@noaa.gov>
2023-08-29 12:18:07 -0500

    Add python load to setup.sh for orion

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-# Use '-i' when sourcing this file to attempt to reset $PATH to system defaults (i.e., based on /etc/profile)
+# Use '-i' when sourcing this file to attempt to reset $PATH to system defaults (i.e., based on /etc/profile),
+# then load any modules (e.g., python) critical for loading and working with Spack.
 # This may have unwanted/unexpected behavior on some systems, so use carefully.
 if [ "$1" == "-i" ]; then
   echo "Resetting PATH to system defaults"
   PATH=$(env -u PATH -u PROFILEREAD bash -c 'echo $PATH')
+  case "$(hostname -s)" in
+    Orion-login-[1-4]) module load python/3.7.5
+    ;;
+  esac
 fi
 
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
