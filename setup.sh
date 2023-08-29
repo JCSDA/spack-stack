a61fa8e3441f1237a51a13845eb1d3bf805f095f
Alex Richert <alexander.richert@noaa.gov>
2023-08-29 09:44:26 -0700

    Add option to setup.sh to create clean environment for installations

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
+# Use '-i' when sourcing this file to attempt to reset $PATH to system defaults (i.e., based on /etc/profile)
+# This may have unwanted/unexpected behavior on some systems, so use carefully.
+if [ "$1" == "-i" ]; then
+  echo "Resetting PATH to system defaults"
+  PATH=$(env -u PATH -u PROFILEREAD bash -c 'echo $PATH')
+fi
+
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
 # Portable way to get current directory
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SPACK_STACK_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export SPACK_STACK_DIR=${SCRIPT_DIR}
+export SPACK_STACK_DIR
 echo "Setting environment variable SPACK_STACK_DIR to ${SPACK_STACK_DIR}"
 
-source ${SCRIPT_DIR}/spack/share/spack/setup-env.sh
-echo "Sourcing spack environment ${SCRIPT_DIR}/spack/share/spack/setup-env.sh"
+source ${SPACK_STACK_DIR}/spack/share/spack/setup-env.sh
+echo "Sourcing spack environment ${SPACK_STACK_DIR}/spack/share/spack/setup-env.sh"
