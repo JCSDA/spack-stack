163bc79414d0252a8524ffe2e29e588d29d8e6ee
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-11-16 20:31:48 -0700

    Avoid confusion in readthedocs by replacing release tag with dev/develop in doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,12 @@ copyright = '2023 '
 author = 'Dominikus Heinzeller, Alexander Richert, Cameron Book'
 
 # The short X.Y version
-version = '1.5'
+#version = '1.5'
+version = 'dev'
+
 # The full version, including alpha/beta/rc tags
-release = '1.5.1'
+#release = '1.5.1'
+release = 'develop'
 
 numfig = True
 
