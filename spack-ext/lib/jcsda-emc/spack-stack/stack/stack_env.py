064ea42b4f5dd343e4dc7287ecbf31e3fd2015f7
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-01-29 16:10:51 -0700

    Replace default_list_scope() with None in spack-ext/lib/jcsda-emc/spack-stack/stack/stack_env.py

--- spack-ext/lib/jcsda-emc/spack-stack/stack/stack_env.py
+++ spack-ext/lib/jcsda-emc/spack-stack/stack/stack_env.py
@@ -271,7 +271,8 @@ class StackEnv(object):
             os.makedirs(env_pkgs_path, exist_ok=False)
             with open(os.path.join(env_repo_path, "repo.yaml"), "w") as f:
                 f.write("repo:\n  namespace: envrepo")
-            repo_paths = spack.config.get("repos", scope=spack.config.default_list_scope())
+            # DH* ???
+            repo_paths = spack.config.get("repos", scope=None) # scope=spack.config.default_list_scope())
             repo_paths = [p.replace("$spack/", spack.paths.spack_root + "/") for p in repo_paths]
             for pkg_name in self.modifypkg:
                 pkg_found = False
