d4532cf3bd00ace62617b66a318facbc3eae3241
Alex Richert <alexander.richert@noaa.gov>
2024-02-16 10:29:24 -0800

    No more bootstrap/cache in ~/.spack

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ echo "Setting environment variable SPACK_STACK_DIR to ${SPACK_STACK_DIR}"
 
 source ${SPACK_STACK_DIR:?}/spack/share/spack/setup-env.sh
 echo "Sourcing spack environment ${SPACK_STACK_DIR}/spack/share/spack/setup-env.sh"
+export SPACK_DISABLE_LOCAL_CONFIG=true
+export SPACK_USER_CACHE_PATH=$SPACK_ROOT/user_cache
 
 # Get the current hash of the spack-stack code
 export SPACK_STACK_HASH=`git rev-parse --short HEAD`
