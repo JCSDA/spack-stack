2ae50c5d34021ce260ddc63dcb8e8ea39efa41db
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-03-08 07:12:33 -0700

    Fix util tests

--- util/util_tests.sh
+++ util/util_tests.sh
@@ -80,7 +80,10 @@ fi
 
 ## Check check_package_config.py
 export SPACK_ENV=${SPACK_STACK_DIR}/util/test_env
-export SPACK_ROOT=${SPACK_STACK_DIR}/spack
+### NOT NEEDED I THINK export SPACK_ROOT=${SPACK_STACK_DIR}/spack
+${SPACK_STACK_DIR}/util/check_package_config.py
+${SPACK_STACK_DIR}/util/check_package_config.py | sort
+${SPACK_STACK_DIR}/util/check_package_config.py | sort | md5sum
 output_checksum=$(${SPACK_STACK_DIR}/util/check_package_config.py | sort | md5sum)
 reference_checksum=$(cat ${SPACK_STACK_DIR}/util/test_env/package_check_baseline.txt | md5sum)
 if [[ "$output_checksum" != "$reference_checksum" ]]; then
