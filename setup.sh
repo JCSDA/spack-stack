c09894224e5fbcb2f1f87defcd6e4b148bf64daf
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-03-20 05:46:52 -0600

    Bug fix in configs/common/modules_*.yaml: need protobuf and py-protobuf modules for cylc-jupyter server to work

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ echo "Setting environment variable SPACK_STACK_DIR to ${SPACK_STACK_DIR}"
 source ${SPACK_STACK_DIR:?}/spack/share/spack/setup-env.sh
 echo "Sourcing spack environment ${SPACK_STACK_DIR}/spack/share/spack/setup-env.sh"
 if [ "$(uname)" != "Darwin" ]; then
-    echo "DO IT"
   export SPACK_USER_CACHE_PATH=$SPACK_ROOT/user_cache
 fi
 
