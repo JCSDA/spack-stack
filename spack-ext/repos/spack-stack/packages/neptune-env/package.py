7669ce9c42c472c26b201778a5a42efce36ef1f0
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-05-03 12:01:45 -0600

    Add neptune-env virtual package for standalone neptune, update spack-ext/repos/spack-stack/packages/jedi-neptune-env/package.py, add configs/templates/neptune-dev

---
+++ spack-ext/repos/spack-stack/packages/neptune-env/package.py
@@ -0,0 +1,34 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class NeptuneEnv(BundlePackage):
+    """Development environment for neptune standalone"""
+
+    # Fake URL
+    homepage = "https://github.com/notavalidaccount/neptune"
+    git = "https://github.com/notavalidaccount/neptune.git"
+
+    maintainers("climbfuji", "areineke")
+
+    version("1.0.0")
+
+    depends_on("base-env", type="run")
+
+    depends_on("libyaml", type="run")
+    depends_on("p4est", type="run")
+    depends_on("w3emc", type="run")
+    depends_on("w3nco", type="run")
+    depends_on("esmf", type="run")
+    depends_on("nco", type="run")
+    depends_on("mct", type="run")
+
+    # Required by ESPC, not used by JEDI
+    depends_on("fftw", type="build")
+    depends_on("netlib-lapack", type="build")
+
+    # There is no need for install() since there is no code.
