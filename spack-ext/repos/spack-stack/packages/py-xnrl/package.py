66fee5d46e76b5b02703e6097a4a63914aa93bc7
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-06-10 07:37:08 -0600

    Configure variant numba (build dependencies that requrire py-numba for py-xnrl

--- spack-ext/repos/spack-stack/packages/py-xnrl/package.py
+++ spack-ext/repos/spack-stack/packages/py-xnrl/package.py
@@ -19,27 +19,12 @@ class PyXnrl(PythonPackage):
 
     version("main", branch="main")
     version("2024.05.23", sha256="73611e72f4a192c9b93039381fdd085c7f1fe09fbdff4bdeb285f744ad2fb05d")
-    #version("2023.10.11", sha256="beff36e3b766449b1a341735b517fbab25331e7363ac21ffc544da284432a482")
-    #version("2022.09.29", sha256="xxxxyyyy509f58f3fe518c12dd5a488c67123fdd66ccb0b968b34fd11e512153")
+
+    variant("numba", default=False, description="Build packages that require py-numba")
 
     depends_on("python@3.9:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    #depends_on("py-poetry@1.6.1", type="build")
     depends_on("py-poetry", type="build")
-
-    #depends_on("eccodes", type=("build", "run"))
-    #depends_on("py-netcdf4@1.6.4:", type=("build", "run"))
-    #depends_on("py-h5netcdf@1.2.0:", type=("build", "run"))
-    #depends_on("py-numpy@1.25.2:", type=("build", "run"))
-    #depends_on("py-xarray@2023.9.0:", type=("build", "run"))
-    #depends_on("py-metpy@1.5.1:", type=("build", "run"))
-    #depends_on("py-dask@2023.9.3: +delayed", type=("build", "run"))
-    #depends_on("py-netcdf4", type=("build", "run"))
-    #depends_on("py-h5netcdf", type=("build", "run"))
-    #depends_on("py-numpy", type=("build", "run"))
-    #depends_on("py-xarray", type=("build", "run"))
-    #depends_on("py-metpy", type=("build", "run"))
-    #depends_on("py-dask +delayed", type=("build", "run"))
 
     #depends_on("py-metpy@1.4.1:", type=("build", "run"))
     depends_on("py-metpy", type=("build", "run"))
@@ -63,7 +48,7 @@ class PyXnrl(PythonPackage):
     depends_on("py-cfgrib", type=("build", "run"))
     #depends_on("py-cf-xarray@0.8.4:", type=("build", "run"))
     depends_on("py-cf-xarray", type=("build", "run"))
-    # does not exist, deprecated:
+    # Does not exist in spack, and is deprecated:
     #depends_on("py-pygrib@2.1.4:", type=("build", "run"))
     #depends_on("py-cartopy@0.22.0:", type=("build", "run"))
     depends_on("py-cartopy", type=("build", "run"))
@@ -71,22 +56,18 @@ class PyXnrl(PythonPackage):
     depends_on("py-cftime", type=("build", "run"))
     #depends_on("py-h5py@3.11.0:", type=("build", "run"))
     depends_on("py-h5py", type=("build", "run"))
-    # stdlib:
-    #depends_on("importlib@1.0.4:", type=("build", "run"))
     #depends_on("py-matplotlib@3.9.0:", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
     #depends_on("py-pandas@2.2.2:", type=("build", "run"))
-    ### Turn off performance variant to avoid py-numba and llvm compiler dependency
-    depends_on("py-pandas ~performance", type=("build", "run"))
-    #depends_on("py-pandas", type=("build", "run"))
+    # Turn off performance variant to avoid py-numba and llvm compiler dependency
+    depends_on("py-pandas ~performance", type=("build", "run"), when="~numba")
+    depends_on("py-pandas +performance", type=("build", "run"), when="+numba")
     #depends_on("py-scipy@1.13.1:", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
-    # TRY WITHOUT xESMF
-    #depends_on("py-xesmf@0.8.5:", type=("build", "run"))
-    ### depends_on("py-xesmf", type=("build", "run"))
-    # TRY WITHOUT BECAUSE OF NUMBA
-    #depends_on("py-xskillscore@0.0.26:", type=("build", "run"))
-    #depends_on("py-xskillscore", type=("build", "run"))
+    #depends_on("py-xesmf@0.8.5:", type=("build", "run"), when="+numba")
+    depends_on("py-xesmf", type=("build", "run"), when="+numba")
+    #depends_on("py-xskillscore@0.0.26:", type=("build", "run"), when="+numba")
+    depends_on("py-xskillscore", type=("build", "run"), when="+numba")
 
     #def url_for_version(self, version):
     #    if version == Version("2022.09.29"):
