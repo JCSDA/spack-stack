9d2aeeeb6bdd5a6ca33b2cd787fb9a4305beeea7
Alex Richert <alexander.richert@noaa.gov>
2024-07-31 15:33:05 -0700

    git clone --recurse-submodules

--- util/weekly_build/2_GetSpackStackDevelop.sh
+++ util/weekly_build/2_GetSpackStackDevelop.sh
@@ -5,4 +5,4 @@ if [ -z $SETUPDONE ]; then . ShellSetup.sh $* ; fi
 set -e
 
 cd $RUNDIR
-git clone https://github.com/JCSDA/spack-stack -b develop spack-stack-build-cache-${RUNID}
+git clone --recurse-submodules https://github.com/JCSDA/spack-stack -b develop spack-stack-build-cache-${RUNID}
