f636b599fc491b14aaa48401e667e6d4e62bfaf9
Alex Richert <alexander.richert@noaa.gov>
2024-07-31 15:36:19 -0700

    allow use of job schedulers for 'spack install'

--- util/weekly_build/4_SpackInstall.sh
+++ util/weekly_build/4_SpackInstall.sh
@@ -14,7 +14,7 @@ for compiler in $COMPILERS; do
   cd envs/build-$compiler
   spack env activate .
   # Just install the packages we're testing (+dependencies):
-  spack install $installopts --test root $PACKAGESTOTEST 2>&1 | tee log.install_withtesting
+  $SCHEDULER_CMD spack install $installopts --test root $PACKAGESTOTEST 2>&1 | tee log.install_withtesting
   # Install the rest of the stack as usual:
-  spack install $installopts 2>&1 | tee log.install
+  $SCHEDULER_CMD spack install $installopts 2>&1 | tee log.install
 done
