703d61db62e4410f1cb68b7375623352c5d23fb7
Alex Richert <alexander.richert@noaa.gov>
2024-07-31 19:35:40 -0700

    misc update

--- util/weekly_build/2_GetSpackStackDevelop.sh
+++ util/weekly_build/2_GetSpackStackDevelop.sh
@@ -5,6 +5,9 @@ if [ -z $SETUPDONE ]; then . ShellSetup.sh $* ; fi
 set -ex
 
 cd $RUNDIR
-if [ ! -d $RUNID ]; then
+if [ -d $RUNID ]; then
+  cd $RUNID
+  git pull
+else
   git clone --recurse-submodules https://github.com/AlexanderRichert-NOAA/spack-stack -b weekly_build $RUNID
 fi
