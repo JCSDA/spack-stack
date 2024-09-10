fbea7f971f4086195463eb3f72a61a37ae8bfe54
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2024-09-10 20:07:17 +0000

    add log files and limited fetching to 4_SpackInstall.sh

--- util/weekly_build/4_SpackInstall.sh
+++ util/weekly_build/4_SpackInstall.sh
@@ -15,9 +15,13 @@ INSTALL_OPTS="--show-log-on-error --fail-fast $INSTALL_OPTS"
 for compiler in $COMPILERS; do
   cd $RUNDIR/$RUNID/envs/build-${compiler/@/-}
   spack env activate .
-  spack fetch
+  if [ -z $PACKAGES_TO_INSTALL ]; then
+    spack fetch 2>&1 | tee log.fetch
+  else
+    spack fetch --dependencies $PACKAGES_TO_INSTALL 2>&1 | tee log.fetch
+  fi
   # Just install the packages we're testing (+dependencies):
-  spack_install_exe install $INSTALL_OPTS --test root $PACKAGES_TO_TEST
+  spack_install_exe install $INSTALL_OPTS --test root $PACKAGES_TO_TEST 2>&1 | tee log.test.install
   # Install the rest of the stack as usual:
-  spack_install_exe install $INSTALL_OPTS $PACKAGES_TO_INSTALL
+  spack_install_exe install $INSTALL_OPTS $PACKAGES_TO_INSTALL 2>&1 | tee log.install
 done
