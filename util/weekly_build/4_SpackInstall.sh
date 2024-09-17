0f3de3292754d6e7d7a4a0dccacf91c3c7522e14
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2024-09-17 01:35:49 +0000

    update fetch logic

--- util/weekly_build/4_SpackInstall.sh
+++ util/weekly_build/4_SpackInstall.sh
@@ -15,10 +15,10 @@ INSTALL_OPTS="--show-log-on-error --fail-fast $INSTALL_OPTS"
 for compiler in $COMPILERS; do
   cd $RUNDIR/$RUNID/envs/build-${compiler/@/-}
   spack env activate .
-  if [ -z $PACKAGES_TO_INSTALL ]; then
-    spack fetch 2>&1 | tee log.fetch
+  if [ -z "$PACKAGES_TO_INSTALL" ]; then
+    spack fetch --missing 2>&1 | tee log.fetch
   else
-    spack fetch --dependencies $PACKAGES_TO_INSTALL 2>&1 | tee log.fetch
+    spack fetch --missing --dependencies $PACKAGES_TO_INSTALL 2>&1 | tee log.fetch
   fi
   # Just install the packages we're testing (+dependencies):
   spack_install_exe install $INSTALL_OPTS --test root $PACKAGES_TO_TEST 2>&1 | tee log.test.install
