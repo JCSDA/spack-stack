720e0ec496c8486c7b34909d285fe029653e746a
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2024-09-24 23:16:48 +0000

    clone faster

--- util/weekly_build/02_GetSpackStack.sh
+++ util/weekly_build/02_GetSpackStack.sh
@@ -9,5 +9,5 @@ if [ -d $RUNID ]; then
   cd $RUNID
   git pull
 else
-  git clone --recurse-submodules ${SPACK_STACK_URL:-https://github.com/AlexanderRichert-NOAA/spack-stack} -b ${SPACK_STACK_BRANCH:-weekly_build} $RUNID
+  git clone --depth 1 --recurse-submodules --shallow-submodules ${SPACK_STACK_URL:-https://github.com/AlexanderRichert-NOAA/spack-stack} -b ${SPACK_STACK_BRANCH:-weekly_build} $RUNID
 fi
