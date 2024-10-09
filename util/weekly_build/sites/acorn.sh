5068f19444eaa1564c66a6e9ff751bf7fac16be6
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2024-10-09 22:00:16 +0000

    use shorter padded length on acorn because of intel19 segfault w/esmf

--- util/weekly_build/sites/acorn.sh
+++ util/weekly_build/sites/acorn.sh
@@ -15,10 +15,11 @@ function spack_install_exe {
   shift 1
   ${SPACK_STACK_DIR}/util/parallel_install.sh 3 4 $*
 }
-PACKAGES_TO_TEST="libpng libaec jasper w3emc g2c"
-PACKAGES_TO_INSTALL="ufs-weather-model-env global-workflow-env gsi-env madis"
 function alert_cmd {
   module purge # annoying libstdc++ issue
   mail -s 'spack-stack weekly build failure' alexander.richert@noaa.gov  < <(echo "Weekly spack-stack build failed in $1. Run ID: $RUNID")
 }
+PACKAGES_TO_TEST="libpng libaec jasper w3emc g2c"
+PACKAGES_TO_INSTALL="ufs-weather-model-env global-workflow-env gsi-env madis"
+PADDED_LENGTH=140
 TEST_UFSWM=ON
