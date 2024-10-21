84d8f78692067cafd159fb153d0ff7bedc57e540
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-10-21 10:36:44 -0600

    Add TEMPLATES list and loop over templates; rename util/weekly_build/03_SetupUnifiedEnv.sh to util/weekly_build/03_SetupEnv.sh

--- util/weekly_build/sites/nautilus.sh
+++ util/weekly_build/sites/nautilus.sh
@@ -1,6 +1,6 @@
 COMPILERS=${COMPILERS:-"intel oneapi gcc"}
+TEMPLATES=${TEMPLATES:-"neptune-dev unified-dev"}
 module purge
 umask 0022
 SPACK_STACK_URL=https://github.nrlmry.navy.mil/JCSDA/spack-stack
 SPACK_STACK_BRANCH=feature/weekly_build_nautilus
-### NEEDED ? BUILD_CACHE_DIR=${BUILD_CACHE_DIR:-/p/app/projects/NEPTUNE/spack-stack/build-cache}
