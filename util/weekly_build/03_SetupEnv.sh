30aa75825bf256b0137bc99e7c0af5f8c6cb8047
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-10-21 10:52:03 -0600

    Set envname and envdir, only perform packages_to_test install step if variable defined

--- util/weekly_build/03_SetupEnv.sh
+++ util/weekly_build/03_SetupEnv.sh
@@ -12,10 +12,12 @@ set -x
 
 for compiler in $COMPILERS; do
   for template in $TEMPLATES; do
-    echo "Setting up environment build-${compiler/@/-} using template $template"
-    rm -rf $RUNDIR/$RUNID/envs/build-${compiler/@/-}
+    envname=build-$template-${compiler/@/-}
+    envdir=$RUNDIR/$RUNID/envs/build-$envname
+    echo "Setting up environment $envname in $envdir"
+    rm -rf $envdir
     spack stack create env --name build-${template}-${compiler/@/-} --template $template --site $PLATFORM --compiler $compiler
-    cd $RUNDIR/$RUNID/envs/build-${compiler/@/-}
+    cd $envdir
     spack env activate .
     spack config add "config:install_tree:padded_length:${PADDED_LENGTH:-200}"
     # Check for duplicates and fail before doing the "real" concretization:
@@ -24,10 +26,10 @@ for compiler in $COMPILERS; do
     spack concretize --fresh --force
 #   The following is not working at the moment, for seemingly a couple reasons. Therefore packages with test-only deps cannot be tested.
 #    spack concretize --force --fresh --test all 2>&1 | tee log.concretize_test
-    # Get path to local-source mirror from spack site config
-    SOURCE_MIRROR=`spack mirror list | grep local-source`
-    SOURCE_MIRROR=${SOURCE_MIRROR/local-source \[sb\] file:\/\//}
-    # Download all source codes needed to build the environment
-    spack mirror create -a -d ${SOURCE_MIRROR}
+###    # Get path to local-source mirror from spack site config
+###    SOURCE_MIRROR=`spack mirror list | grep local-source`
+###    SOURCE_MIRROR=${SOURCE_MIRROR/local-source \[sb\] file:\/\//}
+###    # Download all source codes needed to build the environment
+###    spack mirror create -a -d ${SOURCE_MIRROR}
   done
 done
--- util/weekly_build/04_SpackInstall.sh
+++ util/weekly_build/04_SpackInstall.sh
@@ -10,9 +10,6 @@ set +x
 . setup.sh
 set -x
 
-<<<<<<< HEAD
-INSTALL_OPTS="--show-log-on-error --fail-fast --no-cache $INSTALL_OPTS"
-=======
 if [ "$REUSE_BUILD_CACHE" == YES ]; then
   cache_flag="--no-check-signature"
 else
@@ -20,32 +17,25 @@ else
 fi
 
 INSTALL_OPTS="--show-log-on-error --fail-fast $cache_flag $INSTALL_OPTS"
->>>>>>> 5068f19444eaa1564c66a6e9ff751bf7fac16be6
 
 for compiler in $COMPILERS; do
-  cd $RUNDIR/$RUNID/envs/build-${compiler/@/-}
-  spack env activate .
-<<<<<<< HEAD
-  #spack fetch # 2>&1 | tee log.fetch
-  #if [ -z "$PACKAGES_TO_INSTALL" ]; then
-  #  spack fetch --missing # 2>&1 | tee log.fetch
-  #else
-  #  spack fetch --missing --dependencies $PACKAGES_TO_INSTALL # 2>&1 | tee log.fetch
-  #fi
-  # Just install the packages we're testing (+dependencies):
-  spack_install_exe install $INSTALL_OPTS --test root $PACKAGES_TO_TEST # 2>&1 | tee log.test.install
-  # Install the rest of the stack as usual:
-  spack_install_exe install $INSTALL_OPTS $PACKAGES_TO_INSTALL # 2>&1 | tee log.install
-=======
-  if [ "${SOURCE_CACHE::7}" == "file://" ]; then
-    mirrorpath=${SOURCE_CACHE}
-  else
-    mirrorpath=$(spack mirror list | awk "{if (\$1==\"$SOURCE_CACHE\") print \$NF}")
-  fi
-  spack mirror create --dependencies --directory ${mirrorpath?"Source mirror path could not be determined. Check site's mirrors.yaml."} ${PACKAGES_TO_INSTALL:---all} 2>&1 | tee log.fetch
-  # Just install the packages we're testing (+dependencies):
-  spack_install_exe install $INSTALL_OPTS --test root $PACKAGES_TO_TEST
-  # Install the rest of the stack as usual:
-  spack_install_exe install $INSTALL_OPTS $PACKAGES_TO_INSTALL
->>>>>>> 5068f19444eaa1564c66a6e9ff751bf7fac16be6
+  for template in $TEMPLATES; do
+    envname=build-$template-${compiler/@/-}
+    envdir=$RUNDIR/$RUNID/envs/build-$envname
+    echo "Building environment $envname in $envdir"
+    cd $envdir
+    spack env activate .
+    if [ "${SOURCE_CACHE::7}" == "file://" ]; then
+      mirrorpath=${SOURCE_CACHE}
+    else
+      mirrorpath=$(spack mirror list | awk "{if (\$1==\"$SOURCE_CACHE\") print \$NF}")
+    fi
+    spack mirror create --dependencies --directory ${mirrorpath?"Source mirror path could not be determined. Check site's mirrors.yaml."} ${PACKAGES_TO_INSTALL:---all} 2>&1 | tee log.fetch
+    # Just install the packages we're testing (+dependencies):
+    if [[ -z "${PACKAGES_TO_TEST}" ]]; then
+      spack_install_exe install $INSTALL_OPTS --test root $PACKAGES_TO_TEST
+    fi
+    # Install the rest of the stack as usual:
+    spack_install_exe install $INSTALL_OPTS $PACKAGES_TO_INSTALL
+  done
 done
