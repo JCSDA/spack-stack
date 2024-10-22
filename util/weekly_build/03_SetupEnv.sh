edab0f563178a0c4342e91bc3b3d42013db647e8
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-10-22 14:57:53 -0600

    Add rogue cmd line argument for spack concretize for debugging in util/weekly_build/03_SetupEnv.sh

--- util/weekly_build/03_SetupEnv.sh
+++ util/weekly_build/03_SetupEnv.sh
@@ -21,7 +21,7 @@ for compiler in $COMPILERS; do
     spack env activate .
     spack config add "config:install_tree:padded_length:${PADDED_LENGTH:-200}"
     # Check for duplicates and fail before doing the "real" concretization:
-    spack concretize --fresh | tee log.concretize
+    spack concretize --fresh --somethingnotsupported > log.concretize 2>&1
     ${SPACK_STACK_DIR:?}/util/show_duplicate_packages.py log.concretize -d -i crtm -i esmf
     spack concretize --fresh --force
 #   The following is not working at the moment, for seemingly a couple reasons. Therefore packages with test-only deps cannot be tested.
