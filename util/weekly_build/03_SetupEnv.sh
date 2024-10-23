c9d0fb2827455ac5865c7db05474791143f2f21c
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-10-22 20:42:34 -0600

    Remove debugging code

--- util/weekly_build/03_SetupEnv.sh
+++ util/weekly_build/03_SetupEnv.sh
@@ -22,11 +22,8 @@ for compiler in $COMPILERS; do
     spack config add "config:install_tree:padded_length:${PADDED_LENGTH:-200}"
     # Check for duplicates and fail before doing the "real" concretization:
     #spack concretize --fresh --somethingnotsupported > log.concretize 2>&1
-    spack_wrapper log.concretize concretize --fresh --somethingnotsupported
+    spack_wrapper log.concretize concretize --fresh
     ${SPACK_STACK_DIR:?}/util/show_duplicate_packages.py log.concretize -d -i crtm -i esmf
-    
-    echo "EXIT HERE FOR TESTING"
-    exit 1
     spack concretize --fresh --force
 #   The following is not working at the moment, for seemingly a couple reasons. Therefore packages with test-only deps cannot be tested.
 #    spack concretize --force --fresh --test all | tee log.concretize_test
