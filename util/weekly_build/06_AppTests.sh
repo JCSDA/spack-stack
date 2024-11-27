ae7914a5cb8080029aba6b3528b34572eb58bb66
Rick Grubin <Richard.Grubin@noaa.gov>
2024-11-27 11:49:39 -0600

    WIP: run one UFS WM test

--- util/weekly_build/06_AppTests.sh
+++ util/weekly_build/06_AppTests.sh
@@ -4,6 +4,21 @@ set -ex
 
 if [ -z $SETUPDONE ]; then . ShellSetup.sh $* ; fi
 
+cd $RUNDIR/$RUNID
+
+set +x
+. setup.sh
+set -x
+
+for compiler in $COMPILERS; do
+  cd $RUNDIR/$RUNID/envs/build-${compiler/@/-}
+  spack env activate .
+  spack module lmod refresh -y
+  spack stack setup-meta-modules
+done
+
+# TODO: test against all compilers
+# For now, only Intel (see apptests/test_ufswm.sh)
 if [ "$TEST_UFSWM" == ON ]; then
   ./apptests/test_ufswm.sh
 fi
--- util/weekly_build/apptests/test_ufswm.sh
+++ util/weekly_build/apptests/test_ufswm.sh
@@ -3,3 +3,18 @@
 # Do some stuff, and exit non-zero if UFSWM cannot be successfully built and
 # tested (ideally, have a discernably different error condition if there are
 # numerical differences)
+
+cd ${RUNDIR}
+
+# The following need to be cleaned up for proper variable substitution; this is a one-off test
+BRANCH=spack-stack-automation
+
+git clone -b ${BRANCH} --single-branch --recurse-submodules https://github.com/rickgrubin-noaa/ufs-weather-model.git
+cd ufs-weather-model/tests
+
+# -r ==> rocoto ; not strictly necessary, can run without
+./rt.sh -a epic -k -r -n "control_c48 intel"
+
+rc = $?
+return rc
+
