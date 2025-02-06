a1f6b8c7e026bb6051d3be9ac8aba9de764cb472
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-02-06 11:40:41 -0700

    Update NRL batch_install.sh: exclude crtm-fix from duplicate check list
    All in the title.

--- util/nrl/batch_install.sh
+++ util/nrl/batch_install.sh
@@ -591,7 +591,7 @@ for compiler in "${SPACK_STACK_BATCH_COMPILERS[@]}"; do
     fi
 
     # Check for duplicate packages
-    ./util/show_duplicate_packages.py -i crtm -i esmf -d log.concretize.${env_name}.001
+    ./util/show_duplicate_packages.py -i crtm-fix -i crtm -i esmf -d log.concretize.${env_name}.001
 
     # Update local source cache if requested
     if [[ "${update_source_cache}" == "true"* ]]; then
