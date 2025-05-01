ad5594ef61236de0f4a3cf1fa73a6cfda0751bcf
Rick Grubin <Richard.Grubin@noaa.gov>
2025-05-01 18:10:58 -0500

    remove unnecessary debugging

--- util/weekly_build/02_GetSpackStack.sh
+++ util/weekly_build/02_GetSpackStack.sh
@@ -10,6 +10,4 @@ if [ -d $RUNID ]; then
   git pull
 else
   git clone --depth 1 --recurse-submodules --shallow-submodules ${SPACK_STACK_URL:-https://github.com/JCSDA/spack-stack} -b ${SPACK_STACK_BRANCH:-develop} $RUNID
-#  export SPACK_STACK_BRANCH=fix_hercules_orion_intel_config
-#  git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/rickgrubin-noaa/spack-stack -b multiple_ufs_tests $RUNID
 fi
