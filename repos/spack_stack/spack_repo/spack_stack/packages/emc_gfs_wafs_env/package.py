ad9bd39ef01b6199dcbfbf9e2c01a1f94c668b9f
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-07-22 09:03:10 -0600

    Add spack builtin repo as repos/builtin and migrate spack-ext/repos/spack-stack to new api version 2 under repos/spack_stack

---
+++ repos/spack_stack/spack_repo/spack_stack/packages/emc_gfs_wafs_env/package.py
@@ -0,0 +1,30 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.bundle import BundlePackage
+from spack.package import *
+
+
+class EmcGfsWafsEnv(BundlePackage):
+    """Development environment for EMC GFS WAFS"""
+
+    homepage = "https://github.com/NOAA-EMC/EMC_gfs_wafs"
+    git = "https://github.com/NOAA-EMC/EMC_gfs_wafs.git"
+
+    maintainers("AlexanderRichert-NOAA")
+
+    version("1.0.0")
+
+    depends_on("netcdf-fortran")
+    depends_on("netcdf-c")
+    depends_on("bacio")
+    depends_on("w3emc")
+    depends_on("w3nco")
+    depends_on("sp", when="^ip@:4")
+    depends_on("ip")
+    depends_on("g2")
+    depends_on("bufr")
+
+    # There is no need for install() since there is no code.
---
+++ repos/spack_stack/spack_repo/spack_stack/packages/geos_gcm_env/package.py
@@ -0,0 +1,37 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.bundle import BundlePackage
+from spack.package import *
+
+
+class GeosGcmEnv(BundlePackage):
+    """Development environment for GEOS-GCM"""
+
+    homepage = "https://gmao.gsfc.nasa.gov/GEOS_systems"
+    git = "https://github.com/GEOS-ESM/GEOSgcm"
+
+    maintainers("climbfuji", "mathomp4", "Dooruk")
+
+    # Current version
+    version("1.1.0")
+
+    depends_on("base-env", type="run")
+    depends_on("blas", type="run")
+    depends_on("mepo", type="run")
+    depends_on("esmf", type="run")
+    # mapl is built as part of GEOS, don't load;
+    # needs external gftl-shared/fargparse/pflogger
+    # depends_on("mapl", type="run")
+    depends_on("gftl-shared", type="run")
+    depends_on("fargparse", type="run")
+    depends_on("pflogger", type="run")
+    #
+    depends_on("py-numpy", type="run")
+    depends_on("py-pyyaml", type="run")
+    depends_on("py-ruamel-yaml", type="run")
+    depends_on("udunits", type="run")
+
+    # There is no need for install() since there is no code.
