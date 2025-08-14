c3434be015255d7b70581d99994494e7c1564ec4
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-08-14 08:28:40 -0600

    Comment out gsibec and sp in repos/spack_stack/spack_repo/spack_stack/packages/jedi_base_env/package.py

--- repos/spack_stack/spack_repo/spack_stack/packages/jedi_base_env/package.py
+++ repos/spack_stack/spack_repo/spack_stack/packages/jedi_base_env/package.py
@@ -40,7 +40,8 @@ class JediBaseEnv(BundlePackage):
     depends_on("fftw-api", when="+fftw", type="run")
     depends_on("flex", type="run")
     depends_on("git-lfs", type="run")
-    depends_on("gsibec", type="run")
+    # Commented out until update to ip is completed
+    #depends_on("gsibec", type="run")
     depends_on("gsl-lite", type="run")
     depends_on("hdf", when="+hdf4", type="run")
     depends_on("jedi-cmake", type="run")
