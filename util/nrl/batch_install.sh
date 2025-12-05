5c2a594ddd8325de301a5b363fd48692861b7d3e
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-12-05 11:53:24 -0700

    util/nrl/batch_install.sh: add missing '-i fms' to show_duplicate_packages test

--- util/nrl/batch_install.sh
+++ util/nrl/batch_install.sh
@@ -529,7 +529,7 @@ for compiler in "${SPACK_STACK_BATCH_COMPILERS[@]}"; do
     fi
 
     # Check for duplicate packages
-    ./util/show_duplicate_packages.py -i crtm -i crtm-fix -i esmf -i mapl -i neptune-env -i py-cython -i ip
+    ./util/show_duplicate_packages.py -i crtm -i crtm-fix -i esmf -i mapl -i neptune-env -i py-cython -i ip -i fms
 
     # Update local source cache if requested
     if [[ "${update_source_cache}" == "true"* ]]; then
