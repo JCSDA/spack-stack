1b2a435efd717d38f2933add4569563400cd7ee2
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-12-23 05:53:25 -0700

    Apply suggestions from code review

--- util/nrl/batch_install.sh
+++ util/nrl/batch_install.sh
@@ -452,7 +452,7 @@ for compiler in "${SPACK_STACK_BATCH_COMPILERS[@]}"; do
         case ${compiler} in
           gcc-13.3.1)
             module use /project/spack-stack/gcc-13.3.1/modulefiles
-	    module use /project/spack-stack/openmpi-4.1.8/gcc-13.3.1/modulefiles
+            module use /project/spack-stack/openmpi-4.1.8/gcc-13.3.1/modulefiles
 	    ;;
 	esac
         ;;
