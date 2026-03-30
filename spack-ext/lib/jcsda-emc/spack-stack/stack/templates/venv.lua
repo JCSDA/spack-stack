707829719209fbb18f1bc274dbed40c15793c568
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-03-30 12:15:47 -0700

    Update spack-ext/lib/jcsda-emc/spack-stack/stack/templates/venv.lua

--- spack-ext/lib/jcsda-emc/spack-stack/stack/templates/venv.lua
+++ spack-ext/lib/jcsda-emc/spack-stack/stack/templates/venv.lua
@@ -20,13 +20,12 @@ prepend_path("LD_LIBRARY_PATH", "@VENV_ROOT@/lib64")
 
 -- Prompt modification (bash only)
 if (myShellName() == "bash") then
-  if (mode() == "load") then
-    execute{
-      cmd = [[ source @VENV_MODULEDIR@/ps1mod_bash_load.sh @VENV_NAME@ ]]
-    }
-  elseif (mode() == "unload") then
-    execute{
-      cmd = [[ source @VENV_MODULEDIR@/ps1mod_bash_unload.sh @VENV_NAME@ ]]
-    }
-  end
+  execute{
+    cmd = "source @VENV_MODULEDIR@/ps1mod_bash_load.sh @VENV_NAME@",
+    modeA = { "load" }
+  }
+  execute{
+    cmd = "source @VENV_MODULEDIR@/ps1mod_bash_unload.sh @VENV_NAME@",
+    modeA = { "unload" }
+  }
 end
