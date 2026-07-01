2118ec874558bbbde5dd135178421a8d34d31f49
Alex Richert <alexander.richert@noaa.gov>
2026-07-01 12:47:40 -0700

    reconcile deps with ufs_common.lua

--- repos/spack_stack/spack_repo/spack_stack/packages/ufs_weather_model_env/package.py
+++ repos/spack_stack/spack_repo/spack_stack/packages/ufs_weather_model_env/package.py
@@ -8,7 +8,7 @@ from spack.package import *
 
 
 class UfsWeatherModelEnv(BundlePackage):
-    """Development environment for ufs-weathermodel-bundle"""
+    """Development environment for ufs-weather-model"""
 
     homepage = "https://github.com/ufs-community/ufs-weather-model"
     git = "https://github.com/ufs-community/ufs-weather-model.git"
@@ -30,11 +30,18 @@ class UfsWeatherModelEnv(BundlePackage):
 
     depends_on("bacio", type="run")
     depends_on("crtm", type="run")
+    depends_on("esmf~debug", type="run", when="~debug")
+    depends_on("esmf+debug", type="run", when="+debug")
     depends_on("fms +gfs_phys constants=GFS", type="run")
     depends_on("g2", type="run")
     depends_on("g2tmpl", type="run")
+    depends_on("gftl-shared", type="run")
     depends_on("hdf5", type="run")
     depends_on("ip", type="run")
+    depends_on("jasper", type="run")
+    depends_on("libpng", type="run")
+    depends_on("mapl~debug", type="run", when="~debug")
+    depends_on("mapl+debug", type="run", when="+debug")
     depends_on("netcdf-c", type="run")
     depends_on("netcdf-fortran", type="run")
     depends_on("parallelio", type="run")
@@ -42,11 +49,6 @@ class UfsWeatherModelEnv(BundlePackage):
     depends_on("sp", type="run", when="^ip@:4")
     depends_on("w3emc", type="run")
     depends_on("zlib-api", type="run")
-
-    depends_on("esmf~debug", type="run", when="~debug")
-    depends_on("esmf+debug", type="run", when="+debug")
-    depends_on("mapl~debug", type="run", when="~debug")
-    depends_on("mapl+debug", type="run", when="+debug")
 
     depends_on("ufs-pyenv", type="run", when="+python")
     depends_on("cprnc", type="run", when="+ncutils")
