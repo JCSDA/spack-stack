ae3bd8a40b8ee7495b04bb39f5215cede8bc5bbd
AlexanderHrabski-NOAA <Alexander.Hrabski@noaa.gov>
2026-07-14 14:45:57 -0600

    working draft

--- repos/spack_stack/spack_repo/spack_stack/packages/global_workflow_env/package.py
+++ repos/spack_stack/spack_repo/spack_stack/packages/global_workflow_env/package.py
@@ -22,67 +22,114 @@ class GlobalWorkflowEnv(BundlePackage):
     variant("gsi", default=True, description="Build for GSI DA support")
     variant("ci", default=True, description="Build for automated CI testing")
 
+    '''
+    variant("python", default=True, description="Include extra Python packages")
+    variant("ncutils", default=True, description="Include extra NetCDF utilities (cprnc and nccmp)")
+
+    depends_on("cmake", type="run")
+    depends_on("python", type="run")
+
+    depends_on("bacio", type="run")
+    depends_on("crtm", type="run")
+    depends_on("esmf~debug", type="run", when="~debug")
+    depends_on("esmf+debug", type="run", when="+debug")
+    depends_on("fms +gfs_phys constants=GFS", type="run")
+    depends_on("g2", type="run")
+    depends_on("g2tmpl", type="run")
+    depends_on("gftl-shared", type="run")
+    depends_on("hdf5", type="run")
+    depends_on("ip", type="run")
+    depends_on("jasper", type="run")
+    depends_on("libpng", type="run")
+    depends_on("mapl~debug", type="run", when="~debug")
+    depends_on("mapl+debug", type="run", when="+debug")
+    depends_on("netcdf-c", type="run")
+    depends_on("netcdf-fortran", type="run")
+    depends_on("parallelio", type="run")
+    depends_on("scotch", type="run")
+    depends_on("sp", type="run", when="^ip@:4")
+    depends_on("w3emc", type="run")
+    depends_on("zlib-api", type="run")
+
+    depends_on("ufs-pyenv", type="run", when="+python")
+    depends_on("cprnc", type="run", when="+ncutils")
+    depends_on("nccmp", type="run", when="+ncutils")
+    '''
     depends_on("base-env")
+    depends_on("ufs-weather-model-env")
     depends_on("ufs-pyenv")
-    depends_on("prod-util")
-    depends_on("grib-util")
-    depends_on("pigz")
-    depends_on("zstd")
-    depends_on("ip")
-    depends_on("sp", when="^ip@:4")
-    depends_on("ip2")
-    depends_on("gettext")
-    depends_on("sqlite")
-    depends_on("libjpeg")
-    depends_on("jasper")
-    depends_on("bufr")
-    depends_on("libpng")
-    depends_on("bacio")
-    depends_on("openjpeg")
-    depends_on("nco")
-    depends_on("nccmp")
-    depends_on("antlr")
-    depends_on("cdo")
     depends_on("parallel-netcdf")
-    depends_on("netcdf-c")
-    depends_on("netcdf-fortran")
+    depends_on("netcdf-c") #ufs, base-env
+    depends_on("netcdf-fortran") #ufs, base-env
     depends_on("netcdf-cxx")
-    depends_on("gsl")
-    depends_on("esmf")
+    depends_on("nccmp")
+
+    depends_on("py-wxflow")
     depends_on("py-mpi4py")
     depends_on("py-scipy")
     depends_on("py-certifi")
     depends_on("py-markupsafe")
     depends_on("py-jinja2")
     depends_on("py-pytz")
-    depends_on("g2")
-    depends_on("g2tmpl")
-    depends_on("g2c")
-    depends_on("sfcio")
-    depends_on("w3nco")
-    depends_on("w3emc")
-    depends_on("wrf-io")
-    depends_on("nemsio")
-    depends_on("nemsiogfs")
-    depends_on("ncio")
-    depends_on("landsfcutil")
+
+    depends_on("grib-util")
     depends_on("sigio")
     depends_on("wgrib2")
+
+    depends_on("nco")
+    depends_on("prod-util")
+
+    depends_on("antlr")
+    depends_on("pigz")
+    depends_on("zstd")
+    depends_on("gettext")
+    depends_on("sqlite")
+    depends_on("libjpeg")
+    depends_on("openjpeg")
+
+    depends_on("w3nco")
+
+    depends_on("cdo")
+    depends_on("gsl")
+
+    depends_on("ip") #ufs
+    depends_on("sp", when="^ip@:4") #ufs
+    depends_on("jasper") #ufs
+    depends_on("libpng") #ufs
+    depends_on("bacio") #ufs
+    depends_on("esmf") #ufs
+    depends_on("g2") #ufs
+    depends_on("g2tmpl") #ufs
+    depends_on("g2c")
+    depends_on("w3emc") #ufs
+
     depends_on("met", when="+metplus")
     depends_on("metplus", when="+metplus")
-    depends_on("py-wxflow")
     depends_on("uwtools", when="+uwtools")
 
-    depends_on("ufs-weather-model-env")
-
     # GDASApp dependencies
-    depends_on("jedi-fv3-env", when="+gdas")
-    depends_on("jedi-tools-env", when="+gdas")
-    depends_on("jedi-um-env", when="+gdas")
-    depends_on("ioda", when="+gdas")
+    with when("+gdas"):
+        depends_on("jedi-fv3-env", when="+gdas")
+        depends_on("jedi-tools-env", when="+gdas")
+        depends_on("jedi-um-env", when="+gdas")
+        depends_on("ioda", when="+gdas")
+        depends_on("wrf-io")
+        depends_on("nemsio")
+        depends_on("nemsiogfs")
+        depends_on("ncio")
+        depends_on("landsfcutil")
+        depends_on("sfcio")
 
     # GSI dependencies
-    depends_on("gsi-env", when="+gsi")
+    with when("+gsi"):
+        depends_on("gsi-env")
+        depends_on("bufr")
+        depends_on("wrf-io")
+        depends_on("nemsio")
+        depends_on("nemsiogfs")
+        depends_on("ncio")
+        depends_on("landsfcutil")
+        depends_on("sfcio")
 
     # CI dependencies
     depends_on("pygithub", when="+ci")
