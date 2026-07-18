00b6fbf67af809734d216cd392a5e291ba10162b
climbfuji <dom.heinzeller@icloud.com>
2026-07-17 19:41:03 -0600

    Attempt to fix module setup on Derecho for release/2.1

--- configs/sites/tier1/derecho/setup.sh
+++ configs/sites/tier1/derecho/setup.sh
@@ -1,3 +1,4 @@
 module --force purge
-module use /opt/cray/pe/modulefiles
 module use /glade/work/epicufsrt/contrib/spack-stack/derecho/installs/oneapi-2025.3.1/modulefiles
+module load crayenv/25.03
+
