199d318188eb18fb729ccd60f53b2c8b10396a87
Evan Parker <eparker@ucar.edu>
2026-09-11 13:36:55 -0600

    Fix package docs for git-lfs "*_data" repo (#2122)

--- repos/spack_stack/spack_repo/spack_stack/packages/ufo_data/package.py
+++ repos/spack_stack/spack_repo/spack_stack/packages/ufo_data/package.py
@@ -9,7 +9,17 @@ from spack.package import *
 
 
 class UfoData(CMakePackage):
-    """Test data for UFO (Universal Forward Operator)"""
+    """Test data for UFO (Universal Forward Operator)
+
+    NOTE: This test data repository uses git-lfs test which is invoked
+    by 'git clone'/'git checkout' during the fetch stage. Spack's fetch
+    stage runs in its own process environment, *not* in the build
+    environment, therefore a git-lfs build dependency would not ensure a
+    successful clone. A git-lfs executable must be in the PATH of spack's
+    environment  via a local install or a module load call (e.g. 'module
+    load git-lfs'), otherwise fetching fails.
+    - ref: https://github.com/JCSDA/spack-stack/issues/2109.
+    """
 
     homepage = "https://github.com/JCSDA-internal/ufo-data"
     git = "https://github.com/JCSDA-internal/ufo-data.git"
